22a330446b9928171bda6954d9a5d85e35e36d5d
Ad <ad@accelleran.com>
2020-10-23 08:26:34 +0000

    Adding doc and reworking amfController

---
+++ package_everything.sh
@@ -0,0 +1,4 @@
+helm package amf-controller/ cu-up cu-up-controller/ ds-ctrl/ du-controller/ e1-cp/ e1-sctp-up/ f1-ap/ gnb-controller/ ng-ap/ pm-controller/ res-mgr res-mgr-up/ rr-ctrl/ sctp/
+
+mv *.tgz acc-five-g/charts/
+
