747827944886bc4a7066cc63d7b46a51f567104c
ad <ad@accelleran.com>
2020-11-24 15:29:56 +0000

    Updated umbrella charts folder

---
+++ push_local_images.sh
@@ -0,0 +1,52 @@
+docker tag 8c5ec6450734 accelleran/sctpcpappl:978d39f
+docker push accelleran/sctpcpappl:978d39f
+
+docker tag cedd2a8af3e3 accelleran/pmcontrollerappl:978d39f
+docker push accelleran/pmcontrollerappl:978d39f
+
+docker tag cbec40efc777 accelleran/f1apintfappl:978d39f
+docker push accelleran/f1apintfappl:978d39f
+
+docker tag dd37b514af9d accelleran/dsctrlappl:978d39f
+docker push accelleran/dsctrlappl:978d39f
+
+docker tag fc769d1588fd accelleran/rrctrlappl:978d39f
+docker push accelleran/rrctrlappl:978d39f
+
+docker tag db2dbae63f03 accelleran/ngapappl:978d39f
+docker push accelleran/ngapappl:978d39f
+
+docker tag 99db957cf817 accelleran/e1sctpupappl:978d39f
+docker push accelleran/e1sctpupappl:978d39f
+
+docker tag 011195946bf8 accelleran/cuupcontrollerappl:978d39f
+docker push accelleran/cuupcontrollerappl:978d39f
+
+docker tag a6c0ac3eb732 accelleran/resmgrupappl:978d39f
+docker push accelleran/resmgrupappl:978d39f
+
+docker tag 81e44352daa8 accelleran/e1cpappl:978d39f
+docker push accelleran/e1cpappl:978d39f
+
+docker tag 3b2882eab022 accelleran/cuupappl:978d39f
+docker push accelleran/cuupappl:978d39f
+
+docker tag 55cf057df339 accelleran/amftesterappl:978d39f
+docker push accelleran/amftesterappl:978d39f
+
+docker tag cb1c40689a71 accelleran/dutesterappl:978d39f
+docker push accelleran/dutesterappl:978d39f
+
+docker tag eb1e8a7bcea9 accelleran/amfcontrollerappl:978d39f
+docker push accelleran/amfcontrollerappl:978d39f
+
+docker tag 8d0bc423e763 accelleran/ducontrollerappl:978d39f
+docker push accelleran/ducontrollerappl:978d39f
+
+docker tag cdfea8a70224 accelleran/gnbcontrollerappl:978d39f
+docker push accelleran/gnbcontrollerappl:978d39f
+
+docker tag 51a3eb7ab0e1 accelleran/resmgrappl:978d39f
+docker push accelleran/resmgrappl:978d39f
+
+
