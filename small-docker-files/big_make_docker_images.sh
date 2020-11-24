6c66694a7700e8cb7af9d9eafe8efb7ec4f04d3d
ezeljkovic <ensar.zeljkovic@uantwerpen.be>
2020-11-24 17:44:26 +0100

    fixing allInOne dockerfile

--- small-docker-files/big_make_docker_images.sh
+++ small-docker-files/big_make_docker_images.sh
@@ -1,1 +1,5 @@
-docker build --add-host sign.ant.accelleran.com:10.188.6.149 --add-host jenkins2.ant.accelleran.com:10.188.6.149 --add-host jenkins.ant.accelleran.com:10.188.6.149 -t 5g-maxi -f Dockerfile.in .
+cp ../../../tools/run-test .
+
+create_lib_folder.sh
+
+docker build --add-host sign.ant.accelleran.com:10.188.6.149 --add-host jenkins2.ant.accelleran.com:10.188.6.149 --add-host jenkins.ant.accelleran.com:10.188.6.149 -t 5g-maxi -f Dockerfile.allInOne .
