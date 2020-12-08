ee80311ca471e6c27a0f54b367b723a749285839
ad <ad@accelleran.com>
2020-12-08 14:17:03 +0000

    Adding values files for each sub application

---
+++ acc-five-g/delete-microservice-test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+helm delete fg-amf-tester-1 fg-amf-tester-2 fg-cu-cp fg-cu-up-1 fg-cu-up-2 fg-du-tester fg-e1-up-1 fg-e1-up-2
---
+++ acc-five-g/run-microservice-test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Starting AMF TESTERS..."
+helm install fg-amf-tester-1 . --values amf-tester-1-values.yaml 
+helm install fg-amf-tester-2 . --values amf-tester-2-values.yaml 
+sleep 5
+echo "Starting CuUps..."
+helm install fg-cu-up-1 . --values cu-up-1-values.yaml 
+helm install fg-cu-up-2 . --values cu-up-2-values.yaml 
+sleep 5
+echo "Starting e1-sctp-ups..."
+helm install fg-e1-up-1 . --values e1-sctp-up-1-values.yaml 
+helm install fg-e1-up-2 . --values e1-sctp-up-2-values.yaml 
+sleep 5
+echo "Starting CuCp..."
+helm install fg-cu-cp . --values cu-cp-values.yaml 
+sleep 5
+echo "Starting DU TESTER..."
+helm install fg-du-tester . --values du-tester-values.yaml 
+echo "Finished!"
