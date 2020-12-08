6be3d612ac5c1a34a31434c952a9bd61a34f144d
ad <ad@accelleran.com>
2020-12-08 15:03:53 +0000

    Fixing IP issues for loadBalancer

--- acc-five-g/run-microservice-test.sh
+++ acc-five-g/run-microservice-test.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+waiting_period=10
+
 echo "Starting AMF TESTERS..."
 helm install fg-amf-tester-1 . --values amf-tester-1-values.yaml 
 helm install fg-amf-tester-2 . --values amf-tester-2-values.yaml 
-sleep 5
+sleep $waiting_period
 echo "Starting CuUps..."
 helm install fg-cu-up-1 . --values cu-up-1-values.yaml 
 helm install fg-cu-up-2 . --values cu-up-2-values.yaml 
-sleep 5
+sleep $waiting_period
 echo "Starting e1-sctp-ups..."
 helm install fg-e1-up-1 . --values e1-sctp-up-1-values.yaml 
 helm install fg-e1-up-2 . --values e1-sctp-up-2-values.yaml 
-sleep 5
+sleep $waiting_period
 echo "Starting CuCp..."
 helm install fg-cu-cp . --values cu-cp-values.yaml 
-sleep 5
+sleep $waiting_period
 echo "Starting DU TESTER..."
 helm install fg-du-tester . --values du-tester-values.yaml 
 echo "Finished!"
