374ab6c9dc27fb5a82b27ed14878628286efc809
ad <ad@accelleran.com>
2020-12-16 11:03:00 +0000

    Trying fix for new verison

--- save_logs_to_files.py
+++ save_logs_to_files.py
@@ -2,29 +2,29 @@ import subprocess
 
 
 pods = """
-fg-amf-tester-1-acc-fiveg-amf-tester-7dfb9968b4-shvrq   1/1     Running     0          5m16s
-fg-amf-tester-2-acc-fiveg-amf-tester-6c687b66cf-xsjrr   1/1     Running     0          5m16s
-fg-cu-cp-acc-fiveg-amf-controller-76f8449b64-j65h8      1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-cu-up-controller-5fdd8fdcc7-6zsst    1/1     Running     0          3m56s
-fg-cu-cp-acc-fiveg-ds-ctrl-77d8989865-bpsvp             1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-du-controller-7f6dd8ccd5-s8h7m       1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-f1-ap-75b94b4446-5qdnr               1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-gnb-controller-6cdc9fd95f-wd6lg      1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-pm-controller-674db665db-7q666       1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-res-mgr-6bd7c88bbb-9pxcq             1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-rr-ctrl-78ddcf8cd4-knm7w             1/1     Running     0          3m55s
-fg-cu-cp-acc-fiveg-sctp-555685679d-7sjzr                1/1     Running     0          3m55s
-fg-cu-up-1-acc-fiveg-cu-up-74b9f7c9d9-86jw2             1/1     Running     0          4m50s
-fg-cu-up-1-acc-fiveg-res-mgr-up-56474469d9-b2xhd        1/1     Running     0          4m50s
-fg-cu-up-2-acc-fiveg-cu-up-57f86c6697-wcwvx             1/1     Running     0          4m49s
-fg-cu-up-2-acc-fiveg-res-mgr-up-68f878f878-xqn7m        1/1     Running     0          4m49s
-fg-du-tester-acc-fiveg-du-tester-988cf759b-qj6q9        1/1     Running     0          3m29s
-fg-e1-cp-1-acc-fiveg-e1-cp-7f88b5968c-lw88x             1/1     Running     0          3m58s
-fg-e1-cp-2-acc-fiveg-e1-cp-6586db989b-t8j4z             1/1     Running     0          3m57s
-fg-e1-up-1-acc-fiveg-e1-sctp-up-67f5576bf5-bzbls        1/1     Running     0          4m24s
-fg-e1-up-2-acc-fiveg-e1-sctp-up-6cb8b4f79b-bgd2l        1/1     Running     0          4m23s
-fg-ng-ap-1-acc-fiveg-ng-ap-9d7c99767-5m54g              1/1     Running     0          3m55s
-fg-ng-ap-2-acc-fiveg-ng-ap-6d8d47857b-pfnvm             1/1     Running     0          3m54s
+fg-amf-tester-1-acc-fiveg-amf-tester-5bc9b84bf6-w58zw   1/1     Running     0          6m46s
+fg-amf-tester-2-acc-fiveg-amf-tester-77db6b5785-nlc7w   1/1     Running     0          6m45s
+fg-cu-cp-acc-fiveg-amf-controller-5669b4944d-nmk5h      1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-cu-up-controller-59cf5bffc5-cscj5    1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-ds-ctrl-5854cc9d79-7qnmd             1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-du-controller-7ff8f47cf5-bgr6b       1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-f1-ap-6d8db757c8-gb576               1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-gnb-controller-5667887997-zcrqv      1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-pm-controller-7786fd7c4-zh2vh        1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-res-mgr-59c78b7458-g82vb             1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-rr-ctrl-546dc9cd44-g676h             1/1     Running     0          5m24s
+fg-cu-cp-acc-fiveg-sctp-797bc488db-zlddt                1/1     Running     0          5m23s
+fg-cu-up-1-acc-fiveg-cu-up-5b89667d7-zvv74              1/1     Running     0          6m19s
+fg-cu-up-1-acc-fiveg-res-mgr-up-77f866bd87-b5skk        1/1     Running     0          6m19s
+fg-cu-up-2-acc-fiveg-cu-up-8f6cddbdc-grhg6              1/1     Running     0          6m19s
+fg-cu-up-2-acc-fiveg-res-mgr-up-7575849bdb-zw56c        1/1     Running     0          6m19s
+fg-du-tester-acc-fiveg-du-tester-8b8bc6b9b-kkbwn        1/1     Running     0          4m57s
+fg-e1-cp-1-acc-fiveg-e1-cp-7577489db5-pprzl             1/1     Running     0          5m27s
+fg-e1-cp-2-acc-fiveg-e1-cp-68cbf55c64-x8x8v             1/1     Running     0          5m25s
+fg-e1-up-1-acc-fiveg-e1-sctp-up-7c64954db5-lxdwp        1/1     Running     0          5m53s
+fg-e1-up-2-acc-fiveg-e1-sctp-up-5f86fd5d44-hlr8z        1/1     Running     0          5m51s
+fg-ng-ap-1-acc-fiveg-ng-ap-68bb49fc69-7gjmj             1/1     Running     0          5m23s
+fg-ng-ap-2-acc-fiveg-ng-ap-8477b67f9b-jsd6b             1/1     Running     0          5m23s
 """
 
 for line in pods.splitlines():
