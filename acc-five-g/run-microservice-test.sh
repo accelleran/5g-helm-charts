05929b4d0f548432a7a1bc231a6a5f66c51ddaa5
ezeljkovic <ensar.zeljkovic@uantwerpen.be>
2020-12-22 11:10:25 +0100

    Updating to GNB commit a685837

--- acc-five-g/run-microservice-test.sh
+++ acc-five-g/run-microservice-test.sh
@@ -11,11 +11,6 @@ helm install fg-cu-up-1 . --values cu-up-1-values.yaml --values order-global-val
 helm install fg-cu-up-2 . --values cu-up-2-values.yaml --values order-global-values.yaml
 sleep $waiting_period
 
-echo "Starting e1-sctp-ups..."
-helm install fg-e1-up-1 . --values e1-sctp-up-1-values.yaml --values order-global-values.yaml
-helm install fg-e1-up-2 . --values e1-sctp-up-2-values.yaml --values order-global-values.yaml
-sleep $waiting_period
-
 echo "Starting E1Cps..."
 helm install fg-e1-cp-1 . --values e1-cp-1-values.yaml --values order-global-values.yaml
 helm install fg-e1-cp-2 . --values e1-cp-2-values.yaml --values order-global-values.yaml
@@ -24,6 +19,11 @@ helm install fg-cu-cp . --values cu-cp-values.yaml --values order-global-values.
 echo "Starting NgAps..."
 helm install fg-ng-ap-1 . --values ng-ap-1-values.yaml --values order-global-values.yaml
 helm install fg-ng-ap-2 . --values ng-ap-2-values.yaml --values order-global-values.yaml
+sleep $waiting_period
+
+echo "Starting e1-sctp-ups..."
+helm install fg-e1-up-1 . --values e1-sctp-up-1-values.yaml --values order-global-values.yaml
+helm install fg-e1-up-2 . --values e1-sctp-up-2-values.yaml --values order-global-values.yaml
 sleep $waiting_period
 
 echo "Starting DU TESTER..."
