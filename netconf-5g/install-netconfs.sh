b441fcdd580511deceafee825cbe2c68195c1a44
ad <ad@accelleran.com>
2021-03-09 13:50:35 +0000

    some weird edits, not sure if important

---
+++ netconf-5g/install-netconfs.sh
@@ -0,0 +1,3 @@
+helm install five-g-netconf-redis-cuup1 . --values redis-cuup-values.yaml
+helm install five-g-netconf-cuup2 . --values cuup-2-values.yaml
+helm install five-g-netconf-cucp1 . --values cucp-values.yaml
