206440677ab57e3a97561811a45ca73bee415cf6
ad <ad@accelleran.com>
2021-03-31 08:16:40 +0000

    values update

--- values_files/run_test.sh
+++ values_files/run_test.sh
@@ -2,4 +2,4 @@
 helm install acc-5g-cu-cp-1 ../acc-5g-cu-cp/ --values lab-kit_5g-cu-cp-1_0-2-0.yaml
 helm install acc-5g-cu-up-1 ../acc-5g-cu-up/ --values lab-kit_5g-cu-up-1_0-2-0.yaml
 helm install acc-5g-cu-up-2 ../acc-5g-cu-up/ --values lab-kit_5g-cu-up-2_0-2-0.yaml 
-#helm install acc-5g-nkafka-5g ../acc-five-g/ --values ../acc-five-g/nkafka-5g-values.yaml --values ../acc-five-g/order-global-values.yaml
+#helm install acc-5g-nkafka-5g ../nkafka-5g/ --values ../acc-five-g/lab-kit_nkafka-5g.yaml --values ../acc-five-g/order-global-values.yaml
